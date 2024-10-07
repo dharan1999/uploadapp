a2f96ebbadaa8286913fe828144e162c76fdd7e6
dharan1999 <dharanthakar96@gmail.com>
2024-10-06 21:10:39 -0400

    dependencies added

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ const app = express()
 app.use(express.static('public'))
 const PORT = 8080
 
-app.listen(PORT ,() => console.log( `Server is listening on port ${PORT}`));
+app.listen(PORT ,() => console.log( `Server is running on http://localhost:${PORT}`));
